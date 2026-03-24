4eb86411cb1329e9f75d98bd288adcfe5367e658
SrDefosse <darioarturo.hdz99@gmail.com>
2026-03-24 12:07:00 -0600

    adicion de GSAP

--- frontend/app/components/lib/gsap.ts
+++ frontend/app/components/lib/gsap.ts
@@ -1,9 +1,9 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SplitText } from "gsap/SplitText";
+import ScrollSmoother from "gsap/ScrollSmoother";
+import ScrollToPlugin from "gsap/ScrollToPlugin";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import SplitText from "gsap/SplitText";
 
 gsap.registerPlugin(
   useGSAP,
